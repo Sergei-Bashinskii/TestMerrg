933c206626118acef0072d4cf67fe9e5b0ba645c
Sergei-Bashinskii <sergei13121390@gmail.com>
2024-09-08 15:35:58 +0300

    Test1

--- src/main/java/com/example/demo/entity/Car.java
+++ src/main/java/com/example/demo/entity/Car.java
@@ -7,48 +7,17 @@ import java.util.Objects;
 @Entity
 public class Car {
 
-    private String color;
-    private int kolosa;
+    private String marka;
 
-    public Car(String color, int kolosa) {
-        this.color = color;
-        this.kolosa = kolosa;
+    public Car(String marka) {
+        this.marka = marka;
     }
 
-    public String getColor() {
-        return color;
+    public String getMarka() {
+        return marka;
     }
 
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    public int getKolosa() {
-        return kolosa;
-    }
-
-    public void setKolosa(int kolosa) {
-        this.kolosa = kolosa;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Car car = (Car) o;
-        return kolosa == car.kolosa && Objects.equals(color, car.color);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(color, kolosa);
-    }
-
-    @Override
-    public String toString() {
-        return "Car{" +
-                "color='" + color + '\'' +
-                ", kolosa=" + kolosa +
-                '}';
+    public void setMarka(String marka) {
+        this.marka = marka;
     }
 }
